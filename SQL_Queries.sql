66b5c50297acf7405b82f474408723ed1dfc2c68
Sam007 <122424295+sam14243@users.noreply.github.com>
2024-09-03 23:36:23 +0530

    Final Updates
    Added Hashing and overall UI updates

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -4,8 +4,3 @@ CREATE TABLE Details(
 	Username varchar(255),
     Password varchar(255)
 );
-INSERT INTO Details values('abc','abc');
-INSERT INTO Details values('acb','acb');
-INSERT INTO Details values('bac','bac');
-INSERT INTO Details values('cab','cab');
-INSERT INTO Details values('cbc','cbc');
--- app.py
+++ app.py
@@ -1,45 +1,65 @@
-from flask import Flask, request, \
-        render_template
-app = Flask(__name__)
+from flask import Flask, request, render_template, redirect, url_for
 import pymysql as pms
-conn = pms.connect(host="localhost", 
+import pickle
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+def db_connection():
+    return pms.connect(host="localhost", 
                    port = 3306, 
                    user="root", 
                    password="mysql",
                    db = "model")
-import pickle
-model = pickle.load(open('model.pkl','rb'))
 
-@app.route("/")
+model = pickle.load(open('model.pkl','rb'))
+print(model)
+@app.route("/", methods=['GET'])
 def main():
     return render_template("Login.html")
 
-@app.route("/login", methods=['Get','Post'])
+@app.route("/login", methods=['Post']) #Add Get if doesn't work
 def log():
-    features = [str(i) for i in (request.form.values())]
-    #pred = model.predict([features])
-    #pred = round(pred[0],2)
-    #print(features)
-    cur = conn.cursor()
-    output = cur.execute("select * from Details WHERE Username = %s AND Password = %s", (features[0], features[1]))
-    check = cur.fetchone()
-    #print(check)
-    if (check):
+    username = request.form.get('exp')
+    password = request.form.get('test')
+    with db_connection() as conn:
+        conn = db_connection()
+        cur = conn.cursor()
+        output = cur.execute("select * from Details WHERE Username = %s", (username,))
+        result = cur.fetchone()
+        print(result, check_password_hash(result[0], password))
+        if result and check_password_hash(result[1], password):
+                return redirect(url_for('predict'))
+        else:
+            data = "Invalid Username or Password!"
+            return render_template("Login.html", msg=data)
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('new_exp')
+        password = request.form.get('new_test')
+        hashed_password = generate_password_hash(password)
         
-        return render_template("Prediction.html")
-    else:
-        data = "Invalid Username or Password!"
-        return render_template("Login.html", msg=data)
+        with db_connection() as conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO Details (Username, Password) VALUES (%s, %s)", (username, hashed_password))
+            conn.commit()
+            return redirect(url_for('main'))
     
-@app.route("/predict", methods=['Get','Post'])
-def pred():
-    pred =  [str(i) for i in (request.form.values())]
-    D = {'mega': '0', 'micro': '1','nano': '2', 'macro': '3'}
-    pred[3] = D[pred[3]]
-    pred = [float(i) for i in pred]
-    out = round(model.predict([pred])[0],2)
-    return render_template("success.html", data=out)
-    #return 1
+    return render_template("Signup.html")
+
+@app.route("/predict", methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        pred =  [str(i) for i in (request.form.values())]
+        D = {'mega': 0, 'micro': 1,'nano': 2, 'macro': 3}
+        pred[3] = D[pred[3]]
+        pred = [float(i) for i in pred]
+        print(pred, model)
+        out = round(model.predict([pred])[0],2)
+        return render_template("Success.html", data=out)
+    return render_template("Prediction.html")
 
 if __name__ == '__main__':
     app.run(host='localhost', port = 5000)
